67c882c2d24201f6285e5578ec0690eb35044e49
Cesar <cesaraugusto23121989@gmail.com>
2026-01-19 17:00:21 -0300

    feat: adiciona arquivo de servidor para framework Express

---
+++ src/infraestructure/http/express/server.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import { UserApp } from "./user.routes.js";
+import { errorMiddleware } from "../../middlewares/errorMidleware.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/users',UserApp);
+
+app.use(errorMiddleware);
+
+app.listen(3000,() => { console.log('Server HTTP rodadndo na porta 3000') });
