1abe2214070a3570d537d503b014365c287c69a2
Cesar <cesaraugusto23121989@gmail.com>
2026-01-19 17:06:57 -0300

    feat: adiciona factory para instanciar classes da aplicacao

---
+++ src/useCases/user/factory.ts
@@ -0,0 +1,13 @@
+import { PrismaRepository } from "../../infraestructure/repositories/prisma.adapter.js";
+import { CreateUserCase } from "./createUser/create.user.case.js";
+import { UserController } from "./createUser/create.user.controller.js";
+
+// Criar usuario
+
+const repository = new PrismaRepository();
+const createuseCase = new CreateUserCase(repository);
+const createUserController = new UserController(createuseCase) 
+
+export {
+    createUserController
+}
