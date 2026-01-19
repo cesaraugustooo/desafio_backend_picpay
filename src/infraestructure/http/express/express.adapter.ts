ef102bcb305fba39d5416709c85051bbd9075873
Cesar <cesaraugusto23121989@gmail.com>
2026-01-19 16:59:59 -0300

    feat: adiciona adaptador de inraestrutura para o framework Express

---
+++ src/infraestructure/http/express/express.adapter.ts
@@ -0,0 +1,22 @@
+import type { IHtppContext, Request } from "../../../interfaces/controller.interface.js";
+import { type Response, type Request as RequestExpress, type NextFunction } from "express";
+
+export class ExpresAdapter implements IHtppContext{
+    constructor(private request: RequestExpress, private response: Response, private nextMiddleware: NextFunction) {}
+
+    getRquest(): Request {
+        return {
+            user: (this.request as any).user,
+            body: this.request.body,
+            params: this.request.params
+        }
+    }
+
+    send(httpStatus: number, data: any): void {
+        this.response.status(httpStatus).json(data)
+    }
+
+    next(middleware: any): void {
+        this.nextMiddleware(middleware);
+    }
+}
