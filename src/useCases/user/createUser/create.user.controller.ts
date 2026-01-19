30f772dd952e094e43a71e061d6bb82ef245d830
Cesar <cesaraugusto23121989@gmail.com>
2026-01-19 19:30:36 -0300

    feat: adiciona DTO para resposta

--- src/useCases/user/createUser/create.user.controller.ts
+++ src/useCases/user/createUser/create.user.controller.ts
@@ -1,5 +1,6 @@
-import type { IHtppContext } from "../../../interfaces/controller.interface.js";
+import type { IHtppContext } from "@interfaces/controller.interface.js";
 import type { CreateUserCase } from "./create.user.case.js";
+import { ResponseCreateUserDTO } from "./create.user.dto.js";
 
 export class UserController {
     constructor (
@@ -12,7 +13,7 @@ export class UserController {
 
             const user = await this.service.register(body);
 
-            ctx.send(200,{message:"registro efetuado com sucesso.",user: user});
+            ctx.send(200,{message:"registro efetuado com sucesso.",user: ResponseCreateUserDTO(user)});
         } catch (error) {
             ctx.next(error);
         }
