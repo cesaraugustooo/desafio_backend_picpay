523baa930ad314da5369b6755c7ae5bfa5588eaa
Cesar <cesaraugusto23121989@gmail.com>
2026-01-19 17:08:59 -0300

    refactor: refatora o codigo para atender melhor os principios de arquitetura limpa

--- src/user/index.ts
+++
@@ -1,7 +0,0 @@
-import { UserController } from "./controllers/user.controller.js";
-import { UserRepository } from "./reposytory/user.repository.js";
-import { UserService } from "./service/user.service.js";
-
-export const repository = new UserRepository()
-export const userService = new UserService(repository)
-export const userController = new UserController(userService);
