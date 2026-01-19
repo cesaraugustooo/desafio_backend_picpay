af2a6145d2bcbba5a9379e66823d4580f53bc14a
Cesar <cesaraugusto23121989@gmail.com>
2026-01-19 19:31:15 -0300

    feat: adiciona interface de Dto para resposta

--- src/useCases/user/createUser/create.user.dto.ts
+++ src/useCases/user/createUser/create.user.dto.ts
@@ -6,3 +6,19 @@ export interface CraeteUserDTO {
     password: string,
     cpf: string
 }
+
+export interface IResponseCreateUserDTO {
+    id: string,
+    name: string,
+    email: string,
+    cpf: string,
+}
+
+export function ResponseCreateUserDTO(data: IResponseCreateUserDTO): IResponseCreateUserDTO{
+    return {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        cpf: data.cpf
+    }
+}
