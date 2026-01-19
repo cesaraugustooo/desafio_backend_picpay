cf19e04c60ab1ea2126f6d2958c4bcd806e93472
Cesar <cesaraugusto23121989@gmail.com>
2026-01-19 17:47:17 -0300

    fix: adiciona tipo de retorno null para findByEmail e findByCpf

--- src/domain/repositorys/repository.interface.ts
+++ src/domain/repositorys/repository.interface.ts
@@ -2,6 +2,6 @@ import type { CreateIUser, User } from "../entitys/User.js";
 
 export interface Irepository {
     create(data: CreateIUser): Promise<User>,
-    findByEmail(email: string): Promise<User>,
-    findByCpf(cpf: string): Promise<User> 
+    findByEmail(email: string): Promise<User | null>,
+    findByCpf(cpf: string): Promise<User | null> 
 }
--- src/infraestructure/repositories/prisma.adapter.ts
+++ src/infraestructure/repositories/prisma.adapter.ts
@@ -6,16 +6,28 @@ import { hash } from "../../utils/bcript.js";
 export class PrismaRepository implements Irepository { 
 
     async create(data: CreateIUser): Promise<User> {
-        const user = await prisma.user.create({data: {data}});
+        const user = await prisma.user.create({data: {...data, saldo: 0}});
 
         return user;
     }
 
-    async findByCpf(cpf: string): Promise<User> {
-        return await prisma.user.findUnique({where:{cpf}})
+    async findByCpf(cpf: string): Promise<User | null> {
+        const user = await prisma.user.findUnique({where:{cpf}});
+
+        if(!user){
+            return null;
+        }
+
+        return user;
     }
 
-    async findByEmail(email: string): Promise<User> {
-        return await prisma.user.findUnique({where:{email}})
+    async findByEmail(email: string): Promise<User | null> {
+        const user = await prisma.user.findUnique({where:{email}});
+
+        if(!user){
+            return null;
+        }
+
+        return user;
     }
 }
