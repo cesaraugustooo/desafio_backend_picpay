fab681423f3e902c56759c64a8d45d7dc031cef0
Cesar <cesaraugusto23121989@gmail.com>
2026-01-19 17:02:22 -0300

    feat: adiciona rotas para o adaptador do Express

---
+++ src/infraestructure/http/express/user.routes.ts
@@ -0,0 +1,13 @@
+import { Router, type NextFunction, type Request as expressRequest, type Response } from "express";
+import { createUserController } from "../../../useCases/user/factory.js";
+import { ExpresAdapter } from "./express.adapter.js";
+import { validated } from "../../middlewares/validated.js";
+import { CreateUserSchema } from "./schemas/create.user.schema.js";
+
+const app: Router = Router()
+
+app.post('/register', validated(CreateUserSchema) ,(req: expressRequest, res: Response, next: NextFunction) => {
+    createUserController.register(new ExpresAdapter(req, res, next))
+});
+
+export { app as UserApp }
