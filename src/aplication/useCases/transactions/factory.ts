be561123e71333348b9aec6c1b191025d987cee7
Cesar <cesaraugusto23121989@gmail.com>
2026-01-21 13:32:01 -0300

    feat: adiciona instancia do case de  Autorizador de Transacao Externo

--- src/aplication/useCases/transactions/factory.ts
+++ src/aplication/useCases/transactions/factory.ts
@@ -3,11 +3,13 @@ import { CreateTransactionCase } from "./createTransaction/create.transaction.ca
 import { PrismaRepository } from "@infra/repositories/prisma/user/prisma.user.adapter.js";
 import { PrismaTransactionManager } from "@infra/repositories/transaction.db.adapter.js";
 import { TransactionController } from "@controllers/transactions/create.transaction.controller.js";
+import { AuthorizeTransactionCase } from "@useCases/services/authorizeTransactionCase/authorize.transaction.case.js";
 
 const TransactionRepository = new PrismaTransactionRepository();
 const UserRepository = new PrismaRepository();
 const TransactionDBManager = new PrismaTransactionManager();
-const createTransactionCase = new CreateTransactionCase(TransactionRepository, UserRepository, TransactionDBManager);
+const AuthorizeTransactionMok = new AuthorizeTransactionCase();
+const createTransactionCase = new CreateTransactionCase(TransactionRepository, UserRepository, TransactionDBManager, AuthorizeTransactionMok);
 
 const CreateTransactionController = new TransactionController(createTransactionCase);
 
