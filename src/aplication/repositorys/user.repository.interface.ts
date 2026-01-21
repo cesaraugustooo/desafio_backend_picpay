f61c58efd8cc15a8af24fab16788cae3966c3896
Cesar <cesaraugusto23121989@gmail.com>
2026-01-21 02:22:52 -0300

    fix: muda tipo de resposta

--- src/aplication/repositorys/user.repository.interface.ts
+++ src/aplication/repositorys/user.repository.interface.ts
@@ -1,9 +1,9 @@
-import type { CreateIUser, ResponseUser, User, ValueInterface } from "../../domain/entitys/User.js";
+import type { CreateIUser, ResponseUser, User, UserByContructor,  } from "../../domain/entitys/User.js";
 
 export interface Irepository {
     create(data: User): Promise<ResponseUser>,
     findByEmail(email: string): Promise<ResponseUser | null>,
-    findByCpf(cpf: string): Promise<ResponseUser | null>, 
-    findById(id: string): Promise<ResponseUser | null>
-    updateUser(id: string, value: ValueInterface, tx?: any): Promise<boolean>
+    findByCpf(cpf: string): Promise<UserByContructor | null>, 
+    findById(id: string): Promise<UserByContructor | null>
+    updateUser(id: string, value: number, tx?: any): Promise<boolean>
 }
