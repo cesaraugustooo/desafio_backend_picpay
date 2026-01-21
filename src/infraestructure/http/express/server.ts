98fc5c129991965c53a08bd9bbbd67b0891d5d35
Cesar <cesaraugusto23121989@gmail.com>
2026-01-21 02:25:47 -0300

    feat: adiciona rota de criar transacao

--- src/infraestructure/http/express/server.ts
+++ src/infraestructure/http/express/server.ts
@@ -1,12 +1,14 @@
 import express from "express"
-import { UserApp } from "./user.routes.js";
+import { UserApp } from "./routes/user/user.routes.js";
 import { errorMiddleware } from "@middlewares/errorMidleware.js";
+import { TransactionApp } from "./routes/transaction/transaction.route.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/users',UserApp);
+app.use('/transaction',TransactionApp);
 
 app.use(errorMiddleware);
 
