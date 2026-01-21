6e175409952c845b4592ad9792c2a5bff196def1
Cesar <cesaraugusto23121989@gmail.com>
2026-01-21 02:26:08 -0300

    feat: adiciona arquivo de rotas da entidade de transacoes

---
+++ src/infraestructure/http/express/routes/transaction/transaction.route.ts
@@ -0,0 +1,11 @@
+import { CreateTransactionController } from "@useCases/transactions/factory.js";
+import { Router, type Request, type Response, type NextFunction } from "express";
+import { ExpresAdapter } from "../../express.adapter.js";
+
+const app = Router()
+
+app.post("",(req: Request, res: Response, next: NextFunction) => {
+    CreateTransactionController.create(new ExpresAdapter(req,res,next))
+});
+
+export { app as TransactionApp }
