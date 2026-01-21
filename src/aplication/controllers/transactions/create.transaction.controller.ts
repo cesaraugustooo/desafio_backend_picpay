21107a4b6eb18a8519f5f9afbecc301d4bf60bb8
Cesar <cesaraugusto23121989@gmail.com>
2026-01-21 02:21:25 -0300

    feat: adiciona controller para UseCase CreateTransactionCase

---
+++ src/aplication/controllers/transactions/create.transaction.controller.ts
@@ -0,0 +1,22 @@
+import type { IHtppContext } from "@controllers/interfaces/controller.interface.js";
+import type { ITransaction } from "@domain/entitys/Transaction.js";
+import type { CreateTransactionCase } from "@useCases/transactions/createTransaction/create.transaction.case.js";
+import type { ResponseTransactionDTO } from "./create.transaction.dto.js";
+
+export class TransactionController {
+    constructor (
+        private readonly service: CreateTransactionCase
+    ) {}
+
+    async create (ctx: IHtppContext) {
+        try {
+            const body = ctx.getRquest().body;
+                
+            const transaction = await this.service.create(body);
+
+            return ctx.send(201,transaction);
+        } catch (error) {
+            ctx.next(error);
+        }
+    }
+}
