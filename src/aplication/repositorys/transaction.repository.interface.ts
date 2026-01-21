a6c3ac352c0b47446937678e174e0f9e230bcbd0
Cesar <cesaraugusto23121989@gmail.com>
2026-01-21 02:22:37 -0300

    fix: remove parametro "payer" nao utilizado no metodo create

--- src/aplication/repositorys/transaction.repository.interface.ts
+++ src/aplication/repositorys/transaction.repository.interface.ts
@@ -1,5 +1,5 @@
 import type { ITransaction, ITransactionCreate } from "@domain/entitys/Transaction.js";
 
 export interface ITransactionRepository {
-    create(data: ITransactionCreate, payer: string, tx: any): Promise<ITransaction>
+    create(data: ITransactionCreate, tx: any): Promise<ITransaction>
 }
