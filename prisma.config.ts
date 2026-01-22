6310c492fddfabf7782294ba6d01a390ea68e5b6
Cesar <cesaraugusto23121989@gmail.com>
2026-01-21 23:50:30 -0300

    fix: corrige erro de tipagens no arquivo de configuracao do prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,12 +3,18 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+const databaseUrl: string = process.env["DATABASE_URL"]
+
+if(!databaseUrl){
+  throw new Error("Database Url Invalido")
+}
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: databaseUrl,
   },
 });
