7184cf66ae25097efcb127f7efc52e77a00afc6d
Cesar <cesaraugusto23121989@gmail.com>
2026-01-22 23:05:14 -0300

    feat: adiciona metodo getCarteira no controller

--- src/aplication/controllers/user/create.user.controller.ts
+++ src/aplication/controllers/user/create.user.controller.ts
@@ -1,21 +1,36 @@
 import type { IHtppContext } from "../../interfaces/controllers/controller.interface.js";
 import type { CreateUserCase } from "@useCases/user/createUser/create.user.case.js";
-import { ResponseCreateUserDTO } from "./create.user.dto.js";
+import { ResponseCreateUserDTO } from "./dtos/create.user.dto.js";
+import type { GetCarteiraUseCase } from "@useCases/user/getCarteira/get.carteira.case.js";
 
 export class UserController {
     constructor (
-        private readonly service: CreateUserCase
+        private readonly createUserCase: CreateUserCase,
+        private readonly getCarteiraCase: GetCarteiraUseCase
     ) {}
 
     async register(ctx: IHtppContext) {
         try {
             const body = ctx.getRquest().body
 
-            const user = await this.service.register(body);
+            const user = await this.createUserCase.register(body);
             
             ctx.send(200,{message:"registro efetuado com sucesso.",user: ResponseCreateUserDTO(user)});
         } catch (error) {
             ctx.next(error);
+        }
+    }
+
+    async getCarteira(ctx: IHtppContext) {
+        try {
+            const user = ctx.getRquest().user; 
+
+            const carteira = this.getCarteiraCase.getCarteira(user);
+
+            return ctx.send(200,carteira);
+
+        } catch (error) {
+            ctx.next(error)
         }
     }
 }
