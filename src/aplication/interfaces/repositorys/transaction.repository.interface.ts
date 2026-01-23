bada803520bf6200e8c13155cfd2d0ac1a24c205
Cesar <cesaraugusto23121989@gmail.com>
2026-01-22 23:06:34 -0300

    feat: adiciona novo metodo getByUser que retorna transacoes por usuario

--- src/aplication/interfaces/repositorys/transaction.repository.interface.ts
+++ src/aplication/interfaces/repositorys/transaction.repository.interface.ts
@@ -1,5 +1,12 @@
-import type { ITransaction, ITransactionCreate } from "@domain/entitys/Transaction.js";
+import type { ITransaction, ITransactionCreate, Transaction } from "@domain/entitys/Transaction.js";
 
+export interface UserByToken {
+    id: string,
+    name: string,
+    email: string,
+    saldo: number
+}
 export interface ITransactionRepository {
-    create(data: ITransactionCreate, tx: any): Promise<ITransaction>
+    create(data: ITransactionCreate, tx: any): Promise<ITransaction>;
+    getByUser(user: UserByToken): Promise<ITransaction[]> 
 }
