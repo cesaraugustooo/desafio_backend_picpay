8836e00989912e235a738eddffa366be7a3bc919
Cesar <cesaraugusto23121989@gmail.com>
2026-01-22 23:08:06 -0300

    feat: adiciona metodo getByUser ao adapter do prisma

--- src/infraestructure/adapters/repositories/prisma/transaction/prisma.transacao.adapter.ts
+++ src/infraestructure/adapters/repositories/prisma/transaction/prisma.transacao.adapter.ts
@@ -1,9 +1,22 @@
-import type { ITransactionCreate, ITransaction } from "@domain/entitys/Transaction.js";
-import type { ITransactionRepository } from "../../../../../aplication/interfaces/repositorys/transaction.repository.interface.js";
+import type { ITransactionCreate, ITransaction, Transaction } from "@domain/entitys/Transaction.js";
+import type { ITransactionRepository, UserByToken } from "@aplication/interfaces/repositorys/transaction.repository.interface.js";
+import { prisma } from "../../../../../../prisma/database.js";
+
+
 
 export class PrismaTransactionRepository implements ITransactionRepository {
 
     async create(data: ITransactionCreate, tx: any): Promise<ITransaction> {
         return await tx.transaction.create({data});
+    }
+
+    async getByUser(user: UserByToken): Promise<ITransaction[]> {
+        return await prisma.transaction.findMany({
+            where: {
+            OR: [
+                {payee_id: user.id},
+                {payer_id: user.id}
+            ]
+        }});
     }
 } 
