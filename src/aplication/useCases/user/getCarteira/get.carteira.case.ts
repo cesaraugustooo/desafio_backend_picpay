7db597c78ccdfd3471b03ed42c48e155553c0dab
Cesar <cesaraugusto23121989@gmail.com>
2026-01-22 23:04:18 -0300

    feat: adiciona case para mostrar carteira do usuario

---
+++ src/aplication/useCases/user/getCarteira/get.carteira.case.ts
@@ -0,0 +1,21 @@
+import type { Carteira } from "@aplication/interfaces/carteira.interface.js";
+import type { ITransactionRepository, UserByToken } from "@aplication/interfaces/repositorys/transaction.repository.interface.js";
+import type { Irepository } from "@aplication/interfaces/repositorys/user.repository.interface.js";
+
+export class GetCarteiraUseCase {
+    constructor(
+        private readonly transactionRepository: ITransactionRepository
+    ) { }
+
+    async getCarteira(user: UserByToken): Promise<Carteira> {
+        const transactions = await this.transactionRepository.getByUser(user);
+
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            saldo: user.saldo,
+            transacoes: transactions
+        }
+    }
+}
